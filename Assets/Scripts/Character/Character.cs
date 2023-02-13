f0271bd400479a3558acf47bafcc28c58edda062
Unreal <scott0202@kaist.ac.kr>
2023-02-14 00:41:43 +0900

    피격 체력 텍스트 수정

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -19,7 +19,7 @@ public class Character : MonoBehaviour
         hp -= delta - defense;
         Debug.LogFormat("get damage {0}", hp);
         HpDecreaseText hpDecreaseText = ObjectPool.Instance<HpDecreaseText>().GetObject().GetComponent<HpDecreaseText>();
-        hpDecreaseText.Init(transform.position, delta);
+        hpDecreaseText.Init(transform.position, delta - defense);
         if (hp <= 0)
         {
             Destroy(gameObject);
